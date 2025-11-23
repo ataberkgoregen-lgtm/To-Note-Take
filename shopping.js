1816f3b39c76641e42563ddcbc0d3da8670f5adb
AtaberkGorgen <20060567@stu.omu.edu.tr>
2025-11-24 00:09:24 +0300

    first commit

---
+++ shopping.js
@@ -0,0 +1,205 @@
+const shoppingList = document.querySelector(".shopping-list");
+const shoppingForm = document.querySelector(".shopping-form");
+const filterButtons = document.querySelectorAll(".filter-buttons button");
+const clearBtn = document.querySelector(".clear");
+
+document.addEventListener("DOMContentLoaded", function () {
+  loadItems();
+  shoppingForm.addEventListener("submit", handleFormSubmit);
+
+  displayAlert();
+
+  for (let button of filterButtons) {
+    button.addEventListener("click", handleFilterSelection);
+  }
+
+  clearBtn.addEventListener("click", clear);
+});
+
+function clear() {
+  shoppingList.innerHTML = "";
+  localStorage.clear("shoppingItems");
+  displayAlert();
+}
+
+function displayAlert() {
+  const isEmpty = shoppingList.querySelectorAll("li").length === 0;
+
+  const alert = document.querySelector(".alert");
+
+  alert.classList.toggle("d-none", !isEmpty);
+}
+
+function saveToLS() {
+  const listItems = shoppingList.querySelectorAll("li");
+  const liste = [];
+
+  for (let li of listItems) {
+    const id = li.getAttribute("item-id");
+    const name = li.querySelector(".item-name").textContent;
+    const completed = li.hasAttribute("item-completed");
+
+    liste.push({ id, name, completed });
+  }
+
+  localStorage.setItem("shoppingItems", JSON.stringify(liste));
+}
+
+function addItem(input) {
+  const newItem = createListItem({
+    id: generateId(),
+    name: input.value,
+    completed: false,
+  });
+
+  shoppingList.appendChild(newItem);
+
+  input.value = "";
+
+  updateFilteredItems();
+  saveToLS();
+  displayAlert();
+}
+
+function generateId() {
+  return Date.now().toString();
+}
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  const input = document.getElementById("item-name");
+
+  if (input.value.trim().length === 0) {
+    alert("yeni değer giriniz:");
+    return;
+  }
+
+  addItem(input);
+}
+
+function loadItems() {
+  // const items = [
+  //     {id:1, name: "Yumurta", completed: false},
+  //     {id:2, name: "Balik", completed: false},
+  //     {id:3, name: "Süt", completed: true}
+  // ];
+
+  const items = JSON.parse(localStorage.getItem("shoppingItems")) || [];
+
+  shoppingList.innerHTML = "";
+
+  for (let item of items) {
+    const li = createListItem(item);
+    shoppingList.appendChild(li); //appendchild bizde node türünde htmlLi elementi ister. Bunu anla.
+  }
+}
+
+function createListItem(item) {
+  //checkbox
+  const input = document.createElement("input");
+  input.type = "checkbox";
+  input.classList.add("form-check-input");
+  input.checked = item.completed;
+  input.addEventListener("change", toggleCopmleted);
+
+  //item
+  const div = document.createElement("div");
+  div.textContent = item.name;
+  div.classList.add("item-name");
+  div.addEventListener("click", openEditMode);
+  div.addEventListener("blur", closeEditMode);
+  div.addEventListener("keydown", cancelEnter);
+
+  //delete icon
+  const deleteIcon = document.createElement("span");
+  deleteIcon.className = "fs-3 bi bi-x text-danger delete-icon";
+  deleteIcon.addEventListener("click", removeItem);
+
+  //li
+  const li = document.createElement("li");
+  li.className = "border rounded p-3 mb-1";
+  li.setAttribute("item-id", item.id);
+
+  li.toggleAttribute("item-completed", item.completed);
+
+  li.appendChild(input);
+  li.appendChild(div);
+  li.appendChild(deleteIcon);
+
+  return li;
+}
+
+function removeItem(e) {
+  const a = e.target.parentElement;
+  shoppingList.removeChild(a);
+  saveToLS();
+  displayAlert();
+}
+
+function toggleCopmleted(e) {
+  const li = e.target.parentElement;
+  li.toggleAttribute("item-completed", e.target.checked);
+  updateFilteredItems();
+  saveToLS();
+}
+
+function openEditMode(e) {
+  const li = e.target.parentElement;
+  if (li.hasAttribute("item-completed") == false) {
+    e.target.contentEditable = true;
+  }
+}
+
+function closeEditMode(e) {
+  e.target.contentEditable = false;
+  saveToLS();
+}
+
+function cancelEnter(e) {
+  if (e.key == "Enter") {
+    e.preventDefault();
+    closeEditMode(e);
+  }
+}
+
+function handleFilterSelection(e) {
+  const filterBtn = e.target;
+
+  for (let button of filterButtons) {
+    button.classList.add("btn-secondary");
+    button.classList.remove("btn-primary");
+  }
+
+  filterBtn.classList.add("btn-primary");
+  filterBtn.classList.remove("btn-secondary");
+
+  filterItems(filterBtn.getAttribute("item-filter"));
+}
+
+function filterItems(filterType) {
+  const li_items = shoppingList.querySelectorAll("li");
+
+  for (let li of li_items) {
+    li.classList.remove("d-flex");
+    li.classList.remove("d-none");
+
+    const completed = li.hasAttribute("item-completed");
+
+    if (filterType == "completed") {
+      // tamamlananalar gösterilecek
+      li.classList.toggle(completed ? "d-flex" : "d-none");
+    } else if (filterType == "incomplete") {
+      // tamamlanmayanları göster
+      li.classList.toggle(completed ? "d-none" : "d-flex");
+    } else {
+      // Hepsini göster
+      li.classList.toggle("d-flex");
+    }
+  }
+}
+
+function updateFilteredItems() {
+  const activeFilter = document.querySelector(".btn-primary[item-filter]");
+  filterItems(activeFilter.getAttribute("item-filter"));
+}
